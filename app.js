d24214904ebedbd6f09d90af52c8226bda690a0b
Blessing <blazeahu123@gmail.com>
2025-04-19 23:59:11 +0100

    Update app.js with compose routes and share post content

--- app.js
+++ app.js
@@ -39,8 +39,12 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res) {
-  console.log(req.body.postText);
-  
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  res.redirect("/");
+  posts.push(post);
 });
 
 
